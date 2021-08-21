2c2f6ff2bcd41a0742abb1e3c63446df818bd110
Anay Wadhera <awadhera@berkeley.edu>
2021-08-21 20:40:37 +0530

    lighthouse: build: Add a script to merge CLO
    Kunmun: Switch over remotes to CodeLinaro
    
    Change-Id: I1d36d27be1bf476c951e04de75c9ea85b80af9ab

---
+++ build/envsetup.sh
@@ -0,0 +1,14 @@
+function __print_lighthouse_functions_help() {
+cat <<EOF
+Additional Lighthouse functions:
+- clomerge:        Utility to merge CLO tags.
+EOF
+}
+
+function clomerge()
+{
+    target_branch=$1
+    set_stuff_for_environment
+    T=$(gettop)
+    python3 $T/vendor/lighthouse/build/tools/merge-clo.py $target_branch
+}
