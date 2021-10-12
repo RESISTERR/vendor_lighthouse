52456557cd838ed7403767fad3d528a7e82363e7
TheScarastic <warabhishek@gmail.com>
2021-10-12 06:09:57 +0000

    lighthouse: soong: Rename fod to udfps
     * now flag is changed to TARGET_SURFACEFLINGER_UDFPS_LIB from TARGET_SURFACEFLINGER_FOD_LIB
    
    Change-Id: I499b31d00a053b865ceaa3b34a9762f980bbf14e

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -18,17 +18,17 @@ SOONG_CONFIG_lighthouseGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_process_sdk_version_override \
-    target_surfaceflinger_fod_lib
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
-TARGET_SURFACEFLINGER_FOD_LIB ?= surfaceflinger_fod_lib
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_lighthouseGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_lighthouseGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_lighthouseGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
-SOONG_CONFIG_lighthouseGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
+SOONG_CONFIG_lighthouseGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 
 # Qualcomm variables
 SOONG_CONFIG_NAMESPACES += aosp_vs_qva
