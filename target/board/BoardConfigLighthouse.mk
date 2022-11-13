764ca6c0e7213b39ff9823b25c051fabb6e7af03
Chris Crump <chriscrawford893@gmail.com>
2022-11-12 19:47:13 -0500

    target: board: Include BoardConfigQcom.mk conditionally
    Change-Id: I0ed8696dea19cfa4e27c5d22052f56508c444591

--- target/board/BoardConfigLighthouse.mk
+++ target/board/BoardConfigLighthouse.mk
@@ -8,8 +8,10 @@
 # and breaks WPA Supplicant compilation.
 DISABLE_EAP_PROXY := true
 
+ifeq ($(call is-board-platform-in-list,$(QCOM_BOARD_PLATFORMS)),true)
 # Include our Qualcomm Board configuration.
 include device/qcom/common/BoardConfigQcom.mk
+endif
 
 # Kernel configuration.
 include vendor/lighthouse/target/board/BoardConfigKernel.mk
